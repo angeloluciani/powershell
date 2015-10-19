977ef8b990a80570193aff04b3209680971058b1
Angelo Luciani <mistereal@gmail.com>
2015-10-19 10:33:28 +0200

    created global_value_item_property.ps1
    created global_value_item_property.ps1

---
+++ global_value_item_property.ps1
@@ -0,0 +1,51 @@
+
+# #############################################################################
+# MAIN PROGRAM
+# #############################################################################
+
+$i =0
+
+while ($i -eq 0) 
+{ 
+Write-Host "╔╦╦╦═╦╗╔═╦═╦══╦═╗"
+Write-Host "║║║║╩╣╚╣═╣║║║║║╩╣"
+Write-Host "╚══╩═╩═╩═╩═╩╩╩╩═╝"
+Write-Host ________________________________________________________________
+Write-Host "|1.  |Select the item property from a file  |" -foreground "green"
+Write-Host ________________________________________________________________
+
+Write-Host "Select a command ==> "
+$a = read-host "Write the number action" 
+
+
+switch ($a) 
+    { 
+        0
+	{
+	$i = 1
+	Write-Host THANKS !!!! -foreground yellow
+	}
+		1
+		{
+		
+		  Write-Host "-match" -foreground "magenta"
+		   if (test-path -path "C:\\Users\\angelo.luciani\\Desktop\\popipopi.txt")
+		   {
+		        $text = Get-Content "C:\\Users\\angelo.luciani\\Desktop\\popipopi.txt"
+		   		foreach ($item in $text) 
+				{
+			       #Write-Host $item
+		           $found = $item -match "\'.*\'"
+				   $spid  = $matches[0]
+		           $b = $spid.Length
+		           $pluto = $spid.Substring(1,$b-2)
+			       #Write-Host "YEAAAHH===>" $pluto -foreground "magenta"
+				   $stringacompleta = "global [val property_"+$pluto+" `"values['"+$pluto+"']"+"`"]"
+				   $stringacompleta >> "C:\\Users\\angelo.luciani\\Desktop\\result.txt"
+				}  
+                
+		}
+	}	
+}
+
+Write-Host Set up COMPLETED -foreground "magenta"
