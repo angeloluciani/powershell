3bdc34c7046dbbe84f1a1e15ecc3b496494eeb11
Angelo Luciani <mistereal@gmail.com>
2015-10-20 14:48:28 +0200

    inserted a part of code to remove old files
    inserted a part of code to remove old files

--- global_value_item_property.ps1
+++ global_value_item_property.ps1
@@ -28,23 +28,26 @@ switch ($a)
 		1
 		{
 		
+	#remove old file result if it present
+		if (test-path -path "C:\\Users\\angelo.luciani\\Desktop\\result.txt")
+		   {
+		   Remove-Item "C:\\Users\\angelo.luciani\\Desktop\\result.txt"
+		   }
+		
 		  Write-Host "-match" -foreground "magenta"
 		   if (test-path -path "C:\\Users\\angelo.luciani\\Desktop\\popipopi.txt")
 		   {
 		        $text = Get-Content "C:\\Users\\angelo.luciani\\Desktop\\popipopi.txt"
 		   		foreach ($item in $text) 
 				{
-			       #Write-Host $item
 		           $found = $item -match "\'.*\'"
 				   $spid  = $matches[0]
 		           $b = $spid.Length
 		           $pluto = $spid.Substring(1,$b-2)
-			       #Write-Host "YEAAAHH===>" $pluto -foreground "magenta"
 				   $stringacompleta = "global [val property_"+$pluto+" `"values['"+$pluto+"']"+"`"]"
 				   $stringacompleta >> "C:\\Users\\angelo.luciani\\Desktop\\result.txt"
-				}  
-                
-		}
+				}                 
+           }
 	}	
 }
 
